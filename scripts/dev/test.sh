1fff64778ee812b60e8928e81e47180d05072183
Gilney Nathanael Mathias <gilney_salvo@hotmail.com>
2023-04-04 09:04:53 -0300

    Alert about the passing of the base pom arg

--- scripts/dev/test.sh
+++ scripts/dev/test.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
 # $1 = Base pom.xml
+if [ -z "$1" ]
+  then
+    echo "Base pom.xml argument not supplied"
+    echo "Ex: sh ./scripts/dev/test.sh ./product-service/pom.xml \n"
+    exit 1
+fi
+
 ./mvnw -f $1 test $2
