0e2fbc2084c1be3ee8c0b9590bfee165ee44a38c
Gilney Nathanael Mathias <gilney_salvo@hotmail.com>
2023-04-04 17:41:09 -0300

    Add tests for the `updateInventoryBySku` route

---
+++ inventory-service/src/test/java/com/microservices/spring/inventoryservice/factories/requests/FakeUpdateInventoryRequestFactory.java
@@ -0,0 +1,35 @@
+package com.microservices.spring.inventoryservice.factories.requests;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.github.javafaker.Faker;
+import com.microservices.spring.inventoryservice.requests.UpdateInventoryRequest;
+
+import lombok.AllArgsConstructor;
+
+@Component
+@AllArgsConstructor
+public class FakeUpdateInventoryRequestFactory {
+
+  private final Faker faker;
+
+  public UpdateInventoryRequest createOne() {
+    return UpdateInventoryRequest.builder()
+        .quantity(faker.random().nextInt(100))
+        .build();
+  }
+
+  public List<UpdateInventoryRequest> createMany(int length) {
+    List<UpdateInventoryRequest> requests = new ArrayList<>();
+
+    for (int i = 0; i < length; i++) {
+      requests.add(createOne());
+    }
+
+    return requests;
+  }
+
+}
---
+++ inventory-service/src/test/java/com/microservices/spring/inventoryservice/integration/UpdateInventoryBySkuTest.java
@@ -0,0 +1,110 @@
+package com.microservices.spring.inventoryservice.integration;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.microservices.spring.inventoryservice.BaseIntegrationTest;
+import com.microservices.spring.inventoryservice.Inventory;
+import com.microservices.spring.inventoryservice.InventoryRepository;
+import com.microservices.spring.inventoryservice.exceptions.InventoryWithThisSkuNotFoundException;
+import com.microservices.spring.inventoryservice.exceptions.ValidationErrorsException;
+import com.microservices.spring.inventoryservice.factories.FakeInventoryFactory;
+import com.microservices.spring.inventoryservice.factories.requests.FakeUpdateInventoryRequestFactory;
+import com.microservices.spring.inventoryservice.requests.UpdateInventoryRequest;
+
+public class UpdateInventoryBySkuTest extends BaseIntegrationTest {
+
+  @Autowired
+  private InventoryRepository inventoryRepository;
+
+  @Autowired
+  private FakeInventoryFactory inventoryFactory;
+
+  @Autowired
+  private FakeUpdateInventoryRequestFactory updateInventoryFactory;
+
+  @Test
+  @DisplayName("Should be able to update inventories by sku")
+  public void shouldBeAbleToUpdateInventoriesBySku() throws JsonProcessingException, Exception {
+    Inventory savedInventory = inventoryFactory.createOne();
+    inventoryRepository.save(savedInventory);
+
+    UpdateInventoryRequest request = updateInventoryFactory.createOne();
+
+    ResultActions resultActions = mvc.perform(put("/api/inventories/{sku}", savedInventory.getSku())
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(request)));
+
+    resultActions
+        // .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.sku").value(savedInventory.getSku()))
+        .andExpect(jsonPath("$.quantity").value(request.getQuantity()));
+
+    Optional<Inventory> updatedInventory = inventoryRepository.findById(savedInventory.getId());
+    Assertions.assertTrue(updatedInventory.isPresent());
+    Assertions.assertEquals(updatedInventory.get().getQuantity(), request.getQuantity());
+  }
+
+  @Test
+  @DisplayName("Should not be able to update inventories with an invalid sku")
+  public void shouldNotBeAbleToUpdateInventoriesWithAnInvalidSku() throws JsonProcessingException, Exception {
+    Inventory savedInventory = inventoryFactory.createOne();
+    inventoryRepository.save(savedInventory);
+
+    UpdateInventoryRequest request = updateInventoryFactory.createOne();
+
+    String sku = savedInventory.getSku() + "_invalid";
+    ResultActions resultActions = mvc.perform(put("/api/inventories/{sku}", sku)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(request)));
+
+    resultActions
+        // .andDo(print())
+        .andExpect(status().isNotFound())
+        .andExpect(jsonPath("$.code").value(InventoryWithThisSkuNotFoundException.CODE));
+
+    Optional<Inventory> updatedInventory = inventoryRepository.findById(savedInventory.getId());
+    Assertions.assertTrue(updatedInventory.isPresent());
+    Assertions.assertNotEquals(updatedInventory.get().getQuantity(), request.getQuantity());
+  }
+
+  @Test
+  @DisplayName("Should not be able to update inventories with data that dont comply by the validations")
+  public void shouldNotBeAbleToUpdateInventoriesWithDataThatDontComplyByTheValidations()
+      throws JsonProcessingException, Exception {
+    Inventory savedInventory = inventoryFactory.createOne();
+    inventoryRepository.save(savedInventory);
+
+    UpdateInventoryRequest request = UpdateInventoryRequest.builder()
+        .quantity(0)
+        .build();
+
+    ResultActions resultActions = mvc.perform(put("/api/inventories/{sku}", savedInventory.getSku())
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(request)));
+
+    resultActions
+        // .andDo(print())
+        .andExpect(status().isUnprocessableEntity())
+        .andExpect(jsonPath("$.code").value(ValidationErrorsException.CODE))
+        .andExpect(jsonPath("$.errors").isMap())
+        .andExpect(jsonPath("$.errors.quantity").isNotEmpty());
+
+    Optional<Inventory> updatedInventory = inventoryRepository.findById(savedInventory.getId());
+    Assertions.assertTrue(updatedInventory.isPresent());
+    Assertions.assertNotEquals(updatedInventory.get().getQuantity(), request.getQuantity());
+  }
+
+}
