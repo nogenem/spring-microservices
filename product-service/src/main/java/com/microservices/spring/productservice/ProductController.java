d09423957c45c90b4566a09e3521660d3df4e0a0
Gilney Nathanael Mathias <gilney_salvo@hotmail.com>
2023-04-03 21:34:51 -0300

    Add basic Swagger doc generation

--- product-service/src/main/java/com/microservices/spring/productservice/ProductController.java
+++ product-service/src/main/java/com/microservices/spring/productservice/ProductController.java
@@ -22,6 +22,11 @@ import com.microservices.spring.productservice.requests.UpdateProductRequest;
 import com.microservices.spring.productservice.responses.PagedProductsResponse;
 import com.microservices.spring.productservice.responses.ProductResponse;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
@@ -30,6 +35,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 @RequestMapping("/api/products")
 @Validated
+@Tag(name = "Product API")
 public class ProductController {
 
   private final ProductService productService;
@@ -37,6 +43,10 @@ public class ProductController {
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
+  @Operation(summary = "Store a product")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "201", description = "Product stored"),
+      @ApiResponse(responseCode = "422", description = "Validation errors", content = @Content) })
   public ProductResponse storeProduct(@Valid @RequestBody StoreProductRequest request) {
     Product product = productService.storeProduct(request);
 
@@ -45,7 +55,12 @@ public class ProductController {
 
   @GetMapping
   @ResponseStatus(HttpStatus.OK)
-  public PagedProductsResponse findAllProductsPaged(@RequestParam(defaultValue = "0") @Min(value = 0) int page,
+  @Operation(summary = "Get a page of products")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "A page of products"),
+      @ApiResponse(responseCode = "422", description = "Validation errors", content = @Content) })
+  public PagedProductsResponse findAllProductsPaged(
+      @RequestParam(defaultValue = "0") @Min(value = 0) int page,
       @RequestParam(defaultValue = "30") @Min(value = 5) int size,
       @RequestParam(defaultValue = "-createdAt") String sort) {
     Sort sortBy = getSortBy(sort);
@@ -58,6 +73,10 @@ public class ProductController {
 
   @GetMapping("/{sku}")
   @ResponseStatus(HttpStatus.OK)
+  @Operation(summary = "Get a product by its sku")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "The product found"),
+      @ApiResponse(responseCode = "404", description = "Product with this sku wasn't found", content = @Content) })
   public ProductResponse findProductBySku(@PathVariable("sku") String sku) {
     Product product = productService.findBySku(sku);
 
@@ -66,6 +85,11 @@ public class ProductController {
 
   @PutMapping("/{sku}")
   @ResponseStatus(HttpStatus.OK)
+  @Operation(summary = "Update a product by its sku")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "The updated product"),
+      @ApiResponse(responseCode = "404", description = "Product with this sku wasn't found", content = @Content),
+      @ApiResponse(responseCode = "422", description = "Validation errors", content = @Content) })
   public ProductResponse updateProductBySku(@PathVariable("sku") String sku,
       @Valid @RequestBody UpdateProductRequest request) {
     Product product = productService.updateProduct(sku, request);
@@ -75,6 +99,10 @@ public class ProductController {
 
   @DeleteMapping("/{sku}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
+  @Operation(summary = "Delete a product by its sku")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "204"),
+      @ApiResponse(responseCode = "404", description = "Product with this sku wasn't found", content = @Content) })
   public void deleteProductBySku(@PathVariable("sku") String sku) {
     productService.deleteProduct(sku);
   }
--- product-service/src/main/java/com/microservices/spring/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/microservices/spring/productservice/ProductServiceApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
 @SpringBootApplication
 @EnableMongoAuditing
+@OpenAPIDefinition
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
