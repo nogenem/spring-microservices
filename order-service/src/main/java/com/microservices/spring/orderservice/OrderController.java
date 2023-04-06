00a80c65ed0dfaccada5590da4c09697fe178b8f
Gilney Nathanael Mathias <gilney_salvo@hotmail.com>
2023-04-06 18:58:07 -0300

    Add basic Swagger doc generation

--- order-service/src/main/java/com/microservices/spring/orderservice/OrderController.java
+++ order-service/src/main/java/com/microservices/spring/orderservice/OrderController.java
@@ -12,6 +12,11 @@ import com.microservices.spring.orderservice.models.Order;
 import com.microservices.spring.orderservice.requests.StoreOrderRequest;
 import com.microservices.spring.orderservice.responses.OrderResponse;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 
@@ -19,6 +24,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 @RequestMapping("/api/orders")
 @Validated
+@Tag(name = "Order API")
 public class OrderController {
 
   private final OrderService orderService;
@@ -26,6 +32,10 @@ public class OrderController {
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
+  @Operation(summary = "Place an order")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "201", description = "Order placed"),
+      @ApiResponse(responseCode = "422", description = "Validation errors", content = @Content) })
   public OrderResponse placeOrder(@Valid @RequestBody StoreOrderRequest request) {
     Order order = orderService.placeOrder(request);
 
--- order-service/src/main/java/com/microservices/spring/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/microservices/spring/orderservice/OrderServiceApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
 @SpringBootApplication
 @EnableJpaAuditing
+@OpenAPIDefinition
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
