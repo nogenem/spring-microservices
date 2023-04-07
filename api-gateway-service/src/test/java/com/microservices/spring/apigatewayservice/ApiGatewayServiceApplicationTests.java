d47035b704b8a6f69e5bb887ad3f81710c184c82
Gilney Nathanael Mathias <gilney_salvo@hotmail.com>
2023-04-07 18:11:42 -0300

    Add basic api gateway service

---
+++ api-gateway-service/src/test/java/com/microservices/spring/apigatewayservice/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.microservices.spring.apigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+class ApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
