8766cf34d91b428edf7801c0ce41179921001063
Gilney Nathanael Mathias <gilney_salvo@hotmail.com>
2023-04-19 09:00:58 -0300

    Stop the command on first error

--- scripts/dev/build-services.sh
+++ scripts/dev/build-services.sh
@@ -4,5 +4,8 @@ for f in *; do
     if [ -d "$f" ] && [[ "$f" == *service ]]; then
         echo -e "\n"Command: docker-compose -f docker-compose.dev.yml build "$f" "$@" "\n"
         docker-compose -f docker-compose.dev.yml build "$f" "$@"
+        if [ $? -ne 0 ]; then
+            break
+        fi
     fi
 done
