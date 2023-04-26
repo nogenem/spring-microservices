3097922668a913ea71a32281e093acaf919ce3a0
Gilney Nathanael Mathias <gilney_salvo@hotmail.com>
2023-04-26 14:18:01 -0300

    Improve the fix for the "Illegal Access" problem

--- api-gateway-service/src/main/java/com/microservices/spring/apigatewayservice/ApiGatewayServiceApplication.java
+++ api-gateway-service/src/main/java/com/microservices/spring/apigatewayservice/ApiGatewayServiceApplication.java
@@ -17,16 +17,27 @@ public class ApiGatewayServiceApplication {
 
 	@PreDestroy
 	public void cleanUp() {
-		Set<Thread> threads = Thread.getAllStackTraces().keySet();
+		// Fix: "Illegal access: this web application instance has been stopped
+		// already."
 
+		Set<Thread> threads = Thread.getAllStackTraces().keySet();
 		for (Thread t : threads) {
-			// Fix: "Illegal access: this web application instance has been stopped
-			// already."
-			t.setContextClassLoader(null);
+			ClassLoader contextClassLoader = t.getContextClassLoader();
+			Class<? extends ClassLoader> contextClassLoaderClass = contextClassLoader != null ? contextClassLoader.getClass()
+					: null;
+			if (contextClassLoader != null && contextClassLoaderClass != null
+					&& contextClassLoaderClass.toString().contains("TomcatEmbeddedWebappClassLoader")) {
+				// The parent is the correct loader:
+				// "org.springframework.boot.devtools.restart.classloader.RestartClassLoader"
+				contextClassLoader = contextClassLoader.getParent();
+				t.setContextClassLoader(contextClassLoader);
+			}
 
 			// System.out.println("--------------------------------------------------");
-			// System.out.printf("%s | %s | %d | %s | %s\n", t.getName(),
-			// t.getState(), t.getPriority(), t.isDaemon(), t.getContextClassLoader());
+			// System.out.printf("%s | %s | %d | %s | %s | %s\n", t.getName(),
+			// t.getState(), t.getPriority(), t.isDaemon(),
+			// contextClassLoaderClass,
+			// contextClassLoader);
 			// System.out.println("--------------------------------------------------");
 		}
 	}
